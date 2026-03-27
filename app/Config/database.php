d08fed2845b86c7cdec3c24944ab7fabecdc8cb3
Sagar Naliyapara <naliyaparaspn@gmail.com>
2026-03-27 13:30:30 +0530

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,14 +1,19 @@
 <?php
 class DATABASE_CONFIG {
+
     public $default = array(
-        'datasource' => 'Database/Mysql', // old CakePHP driver
+        'datasource' => 'Database/Mysqli', // must use Mysqli for MySQL 8
         'persistent' => false,
-        'host' => 'mysql-1791f388-xx-xx.e.aivencloud.com',
-        'login' => 'avnlegacy',
-        'password' => 'xxx',
+        'host' => 'mysql-1791f388-sagarnaliyapara78-b2bb.e.aivencloud.com',
+        'login' => 'avnadmin',
+        'password' => 'AVNS_jOpfMZL8w9gkiKM7MYB',
         'database' => 'defaultdb',
-        'port' => 24663,
+        'port' => 24659,
         'prefix' => '',
         'encoding' => 'utf8',
+        'flags' => MYSQLI_CLIENT_SSL,
+        'ssl_key' => null,
+        'ssl_cert' => null,
+        'ssl_ca' => '/path/to/ca.pem', // Aiven CA certificate
     );
 }
