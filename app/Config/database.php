702aaa85d834a6e807b436c4a67fe48915d1c5bb
Sagar Naliyapara <naliyaparaspn@gmail.com>
2026-03-27 13:09:13 +0530

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,20 +4,12 @@ class DATABASE_CONFIG {
     public $default = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
-        'host' => '',
-        'login' => '',
-        'password' => '',
-        'database' => '',
-        'port' => 3306, // default fallback
+        'host' => getenv('DB_HOST'),
+        'login' => getenv('DB_USER'),
+        'password' => getenv('DB_PASS'),
+        'database' => getenv('DB_NAME'),
+        'port' => getenv('DB_PORT'), // default fallback
         'prefix' => '',
         'encoding' => 'utf8',
     );
-
-    public function __construct() {
-        $this->default['host'] = getenv('DB_HOST');
-        $this->default['login'] = getenv('DB_USER');
-        $this->default['password'] = getenv('DB_PASS');
-        $this->default['database'] = getenv('DB_NAME');
-        $this->default['port'] = getenv('DB_PORT') !== false ? getenv('DB_PORT') : 3306;
-    }
 }
