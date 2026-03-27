0bffed1a0f68366db80ad059de4733722769cce0
Sagar Naliyapara <naliyaparaspn@gmail.com>
2026-03-27 13:19:46 +0530

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -2,7 +2,7 @@
 class DATABASE_CONFIG {
 
     public $default = array(
-        'datasource' => 'Database/Mysql',
+        'datasource' => 'Database/Mysqli',
         'persistent' => false,
         'host' => 'mysql-1791f388-sagarnaliyapara78-b2bb.e.aivencloud.com',
         'login' => 'avnadmin',
@@ -11,5 +11,6 @@ class DATABASE_CONFIG {
         'port' => '24659', // default fallback
         'prefix' => '',
         'encoding' => 'utf8',
+		'flags' => [MYSQLI_CLIENT_SSL], // optional if SSL required
     );
 }
