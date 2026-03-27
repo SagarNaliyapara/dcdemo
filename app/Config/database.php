eb07868007c4d1cb8ac6903ef616ec6fb2322775
Sagar Naliyapara <naliyaparaspn@gmail.com>
2026-03-27 12:27:44 +0530

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,14 +1,18 @@
 <?php
 class DATABASE_CONFIG {
-    public $default = array(
-        'datasource' => 'Database/Mysql',
-        'persistent' => false,
-        'host' => getenv('DB_HOST'),
-        'login' => getenv('DB_USER'),
-        'password' => getenv('DB_PASS'),
-        'database' => getenv('DB_NAME'),
-        'port' => getenv('DB_PORT') ?: 3306,
-        'prefix' => '',
-        'encoding' => 'utf8',
-    );
+    public $default = array();
+
+    public function __construct() {
+        $this->default = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => getenv('DB_HOST'),
+            'login' => getenv('DB_USER'),
+            'password' => getenv('DB_PASS'),
+            'database' => getenv('DB_NAME'),
+            'port' => getenv('DB_PORT') ? getenv('DB_PORT') : 3306,
+            'prefix' => '',
+            'encoding' => 'utf8',
+        );
+    }
 }
