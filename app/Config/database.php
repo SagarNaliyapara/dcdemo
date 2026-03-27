171f978c102d06e027cc8f1b253e8cb611b0b51e
Sagar Naliyapara <naliyaparaspn@gmail.com>
2026-03-27 12:50:18 +0530

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,26 +1,18 @@
 <?php
+$dbPort = getenv('DB_PORT');
+$dbPort = $dbPort !== false ? $dbPort : 3306;
+
 class DATABASE_CONFIG {
 
     public $default = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
-        'host' => null,
-        'login' => null,
-        'password' => null,
-        'database' => null,
-        'port' => 3306,
+        'host' => getenv('DB_HOST'),
+        'login' => getenv('DB_USER'),
+        'password' => getenv('DB_PASS'),
+        'database' => getenv('DB_NAME'),
+        'port' => $dbPort,
         'prefix' => '',
         'encoding' => 'utf8',
     );
-
-    // Use a static initializer to avoid constructor issues
-    public function __construct() {
-        $this->default['host'] = getenv('DB_HOST');
-        $this->default['login'] = getenv('DB_USER');
-        $this->default['password'] = getenv('DB_PASS');
-        $this->default['database'] = getenv('DB_NAME');
-
-        // Use standard ternary (not ?:)
-        $this->default['port'] = getenv('DB_PORT') ? getenv('DB_PORT') : 3306;
-    }
 }
